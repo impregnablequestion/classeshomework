d6fe4fd2a9f2ce639bb87b553289b3570eb354f6
Séamus <ryans40@tcd.ie>
2023-02-06 19:43:03 +0000

    completed calculator tests and methods

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,2 +1,28 @@
+import org.example.Calculator;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class CalculatorTest {
+    Calculator calc;
+    @Before
+    public void setUp () {
+        calc = new Calculator();
+    }
+    @Test
+    public void calculatorHasAdd () {
+        assertEquals(5, calc.add(3,2));
+    }
+    @Test
+    public void calculatorHasSubtract () {
+        assertEquals(5, calc.subtract(8, 3));
+    }
+    @Test
+    public void calculatorHasMultiply () {
+        assertEquals(10, calc.multiply(2, 5));
+    }
+    @Test
+    public void calculatorHasDivide () {
+        assertEquals(2.5, calc.divide(5, 2), 0.00);
+    }
 }
