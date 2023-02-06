e0fa901a15bb37ac9b5b001b111ba55788be3345
Séamus <ryans40@tcd.ie>
2023-02-06 19:53:55 +0000

    completed water bottle tests and methods

--- src/main/java/org/example/WaterBottle.java
+++ src/main/java/org/example/WaterBottle.java
@@ -1,4 +1,19 @@
 package org.example;
-
 public class WaterBottle {
+    int volume;
+    public WaterBottle () {
+        this.volume = 100;
+    }
+    public int getVolume () {
+        return this.volume;
+    }
+    public void drink () {
+        this.volume -= 10;
+    }
+    public void empty () {
+        this.volume = 0;
+    }
+    public void fill () {
+        this.volume = 100;
+    }
 }
--- src/test/java/WaterBottleTest.java
+++ src/test/java/WaterBottleTest.java
@@ -1,2 +1,32 @@
+import org.example.WaterBottle;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 public class WaterBottleTest {
+    WaterBottle chillies;
+    @Before
+    public void setUp () {
+        chillies = new WaterBottle();
+    }
+    @Test
+    public void hasVolume () {
+        assertEquals(100, chillies.getVolume());
+    }
+    @Test
+    public void canDrink () {
+        chillies.drink();
+        assertEquals(90, chillies.getVolume());
+    }
+    @Test
+    public void canEmpty () {
+        chillies.empty();
+        assertEquals(0, chillies.getVolume());
+    }
+    @Test
+    public void canFill () {
+        chillies.empty();
+        chillies.fill();
+        assertEquals(100, chillies.getVolume());
+    }
 }
